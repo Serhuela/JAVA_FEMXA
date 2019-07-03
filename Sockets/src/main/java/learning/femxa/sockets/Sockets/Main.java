bc9049151ef885eae1d229f4407b06018b729607
Serhuela <serhuela@gmail.com>
2019-07-03 11:24:44 +0200

    deleting gradle project.

--- Sockets/src/main/java/learning/femxa/sockets/Sockets/Main.java
+++ Sockets/src/main/java/learning/femxa/sockets/Sockets/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package learning.femxa.socketsSockets;
+package learning.femxa.sockets.Sockets;
 
 /**
  *
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello, World!");
     }
     
 }
