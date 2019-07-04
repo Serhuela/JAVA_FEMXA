7fd1e298c82db40a39cf8b1fdf3c1cc1d2ba3884
Serhuela <serhuela@gmail.com>
2019-07-04 13:18:19 +0200

    Handling fileIO

--- Sockets/src/main/java/learning/femxa/sockets/Sockets/Main.java
+++ Sockets/src/main/java/learning/femxa/sockets/Sockets/Main.java
@@ -5,8 +5,9 @@
  */
 package learning.femxa.sockets.Sockets;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -29,10 +30,28 @@ public class Main {
             fos = new FileOutputStream("datos.dat");
             DataOutputStream dos;
             dos = new DataOutputStream(fos);
-            dos.writeUTF("bla");
+            dos.writeUTF("uno");
+            dos.writeInt(1);
+            dos.writeInt(2);
+            dos.writeUTF("dos");
             dos.flush();
             dos.close();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+        
+        try {
+            FileInputStream fis;
+            fis = new FileInputStream("datos.dat");
+            
+            DataInputStream dis;
+            dis = new DataInputStream(fis);
+            
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readUTF());
+        } catch (IOException e) {
             System.out.println("Error" + e.getMessage());
         }
         
